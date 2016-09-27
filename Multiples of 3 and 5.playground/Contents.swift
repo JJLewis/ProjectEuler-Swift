1fdcb466709cfb598aa95510cf5072a8375bf76b
Jordan Lewis <jordan_j_lewis@icloud.com>
2016-09-27 23:16:11 +1000

    Answer Q1
    Multiples of 3 and 5

--- Multiples of 3 and 5.playground/Contents.swift
+++ Multiples of 3 and 5.playground/Contents.swift
@@ -2,4 +2,22 @@
 
 import UIKit
 
-var str = "Hello, playground"
+func divisibleBy(divisor:Int) -> ((Int) -> Bool) {
+    return { (dividend:Int) -> Bool in
+        return dividend % divisor == 0
+    }
+}
+
+func by3(_ dividend:Int) -> Bool {
+    return divisibleBy(divisor: 3)(dividend)
+}
+
+func by5(_ dividend:Int) -> Bool {
+    return divisibleBy(divisor: 5)(dividend)
+}
+
+let oneToThousand = Array(1..<1000)
+let multiplesOf3Or5 = oneToThousand.filter {by3($0) || by5($0)}
+let sum = multiplesOf3Or5.reduce(0, +)
+
+print(sum)
